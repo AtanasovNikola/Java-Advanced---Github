d8e7b85693c273565eff46ba560d91b457f51692
AtanasovNikola <atanasov_nikola99@abv.bg>
2023-01-11 02:10:06 +0200

    Task: Defining classes

--- src/DefiningClasses/Main.java
+++ src/DefiningClasses/Main.java
@@ -6,15 +6,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numOfCars = Integer.parseInt(scanner.nextLine());
-        while (numOfCars-->0){
-        String [] inputCar = scanner.nextLine().split("\\s+");
-        String brand = inputCar[0];
-        String model = inputCar[1];
-        int horsePower = Integer.parseInt(inputCar[2]);
-        Car car = new Car(brand,model,horsePower);
-        car.setBrand(brand);
-        car.setModel(model);
-        car.setHorsePower(horsePower);
+        while (numOfCars-- > 0) {
+            String[] inputCar = scanner.nextLine().split("\\s+");
+            CarConstructor car;
+            String brand = inputCar[0];
+            if (inputCar.length == 1) {
+                car = new CarConstructor(brand);
+                car.setBrand(brand);
+            } else {
+                String model = inputCar[1];
+                int horsePower = Integer.parseInt(inputCar[2]);
+                car = new CarConstructor(brand, model, horsePower);
+                car.setBrand(brand);
+                car.setHorsePower(horsePower);
+                car.setModel(model);
+
+            }
             System.out.println(car);
         }
     }
